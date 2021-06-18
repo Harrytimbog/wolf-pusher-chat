27f26c2119c8a391f9af806b98df4707900291d9
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-06-18 19:57:12 +0100

    Create chat model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'chats/create'
+  get 'chats/new'
+  get 'chats/show'
+  get 'chats/index'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
